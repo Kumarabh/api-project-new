d98c6503dfa223a9ac9fbeecc4d67aa7ebbacb03
abhishek <abhi@gmail.com>
2020-08-30 09:13:22 +0530

    commit-5

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+
+const connectDB = require('./config/db');
+// load env files
+dotenv.config({path: './config/config.env'});
+
+//connect to database 
+connectDB();
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//Middlewares
+app.use('/api/v1/bootcamps',bootcamps);
+// app.use(logger);
+
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+
+//handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) =>{
+console.log(`Error ${err.messaage}` )
+server.close(() => process.exit(1));
+})
